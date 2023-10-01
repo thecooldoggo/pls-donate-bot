78aef6247573dd124a7a319133bb20482110bbb7
TheDog <130795558+TheDog-bot@users.noreply.github.com>
2023-10-01 14:41:16 -0400

    Update script.py

--- script.py
+++ script.py
@@ -7,15 +7,15 @@ load_dotenv()
 
 async def main():
     print("Welcome to the pls donate bot!")
-    print("Made by @the-dog bot on github")
-    print("Make sure you have the game open and are in the game!")
+    print("Made by @thedog-bot bot on github")
+    print("Make sure you have the game open and are not tabbed out!")
     await asyncio.sleep(3)
     webbrowser.open('https://www.roblox.com/games/8737602449')
     await asyncio.sleep(30)
     while True:
         pyautogui.typewrite("/")
         time.sleep(1)
-        pyautogui.typewrite("WRITE COSTOM MESSAGE 1")
+        pyautogui.typewrite("WRITE CUSTOM MESSAGE 1")
         pyautogui.press("enter")
         time.sleep(15)
         pyautogui.typewrite("/")
