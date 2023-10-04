71b425684358280e9b3b3b6adb6ddeb569a11bf8
TheDog <130795558+TheDog-bot@users.noreply.github.com>
2023-10-04 18:17:00 -0400

    performance improvements + more!
    fixed the misspelling / more messages and made the script shorter

--- script.py
+++ script.py
@@ -1,36 +1,30 @@
-import pyautogui
-import time
 import random
+import time
+import pyautogui
 
-print("Welcome to the pls donate bot!")
-print("Made by @thedog-bot bot on github")
-print("Make sure you have the game open and are not tabbed out!")
+# PUT MESSAGES HERE!
+MESSAGES = [
+    "MESSAGE 1",
+    "MESSAGE 2",
+    "MESSAGE 3",
+    "MESSAGE 4",
+    "MISSEPELLED MESSAGE 1",
+    "MISPELLED MESSAGE 2",
+    "MISPELLED MESSAGE 3",
+    "MISPELLED MESSAGE 4",
+]
+
+print("Make sure you have the chat open and you are in a game!")
 print("Do not put 'robux' or anything like that or else you might get banned!")
+print("Made by @thedog-bot on github.")
 time.sleep(10)
 
-message_count = 0
 while True:
+    # type message in chat
+    message = random.choice(MESSAGES)
     pyautogui.typewrite("/")
     time.sleep(1)
-    pyautogui.typewrite("MESSAGE 1")
+    pyautogui.typewrite(message)
     pyautogui.press("enter")
-    message_count += 1
-    if message_count % random.randint(3, 7) == 0:
-        pyautogui.typewrite("/")
-        time.sleep(1)
-        pyautogui.typewrite("MISSEPELLED MESSAGE 1")
-        pyautogui.press("enter")
-    sleep_time = random.randint(20, 100)
-    time.sleep(sleep_time)
-    pyautogui.typewrite("/")
-    time.sleep(1)
-    pyautogui.typewrite("MESSAGE 2")
-    pyautogui.press("enter")
-    if message_count % random.randint(3, 7) == 0:
-        pyautogui.typewrite("/")
-        time.sleep(1)
-        pyautogui.typewrite("MISPELLED MESSAGE 2")
-        pyautogui.press("enter")
-    message_count += 1
-    sleep_time = random.randint(20, 100)
+    sleep_time = random.randint(20, 110)
     time.sleep(sleep_time)
