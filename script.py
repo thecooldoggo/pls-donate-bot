1a070ddf8093a9b68f6fe26cbdc11fa15fa639e4
TheDog <130795558+TheDog-bot@users.noreply.github.com>
2023-10-10 19:25:03 -0400

    better
    better

--- script.py
+++ script.py
@@ -1,23 +1,34 @@
 import random
 import time
 import pyautogui
+from termcolor import colored
 
 # PUT MESSAGES HERE!
 MESSAGES = [
-    "MESSAGE 1",
-    "MESSAGE 2",
-    "MESSAGE 3",
-    "MESSAGE 4",
-    "MISSEPELLED MESSAGE 1",
-    "MISPELLED MESSAGE 2",
-    "MISPELLED MESSAGE 3",
-    "MISPELLED MESSAGE 4",
+    "hello there!",
+    "/e 1",
+    "donate for the dogs?",
+    "have an amazing day!",
+    "helo there.",
+    "donote fo the dogs?",
+    "Hav an amazin day!",
 ]
 
-print("Make sure you have the chat open and you are in a game!")
-print("Do not put 'robux' or anything like that or else you might get banned!")
-print("Made by @thedog-bot on github.")
-time.sleep(10)
+print(colored('Do not put "robux" or anything like that or else you might get banned!', 'red', 'on_red', ['bold', 'underline']))
+print(colored('Made by @thedog-bot on github.', 'green', 'on_green', ['bold', 'underline']))
+print(colored('Make sure you are in a game! You have 30 before the script starts.', 'yellow', 'on_yellow', ['bold', 'underline']))
+time.sleep(25)
+print(colored('5...', 'red', 'on_red', ['bold']))
+time.sleep(1)
+print(colored('4...', 'red', 'on_red', ['bold']))
+time.sleep(1)
+print(colored('3...', 'red', 'on_red', ['bold']))
+time.sleep(1)
+print(colored('2...', 'red', 'on_red', ['bold']))
+time.sleep(1)
+print(colored('1...', 'red', 'on_red', ['bold']))
+time.sleep(1)
+print(colored('Quit this window to end the script!', 'blue', 'on_blue', ['bold']))
 
 while True:
     # type message in chat
