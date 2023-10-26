eb156d0cc612e8c94ce6b193b68f38b896adec16
TheDog <130795558+TheDog-bot@users.noreply.github.com>
2023-10-26 07:10:52 -0400

    fix color

--- Chat+anti_afk.py
+++ Chat+anti_afk.py
@@ -13,21 +13,21 @@ MESSAGES = [
     "MISPELLED MESSAGE 3",
 ]
 
-print(colored('Do not put "robux" or anything like that or else you might get banned!', 'red', 'on_red', ['bold', 'underline']))
-print(colored('Made by @thedog-bot on github.', 'green', 'on_green', ['bold', 'underline']))
-print(colored('Make sure you are in a game! You have 30 before the script starts.', 'yellow', 'on_yellow', ['bold', 'underline']))
+print(colored('Do not put "robux" or anything like that or else you might get banned!', 'white', 'on_red', ['bold', 'underline']))
+print(colored('Made by @thedog-bot on github.', 'white', 'on_green', ['bold', 'underline']))
+print(colored('Make sure you are in a game! You have 30 before the script starts.', 'white', 'on_yellow', ['bold', 'underline']))
 time.sleep(25)
-print(colored('5...', 'red', 'on_red', ['bold']))
+print(colored('5...', 'white', 'on_red', ['bold']))
 time.sleep(1)
-print(colored('4...', 'red', 'on_red', ['bold']))
+print(colored('4...', 'white', 'on_red', ['bold']))
 time.sleep(1)
-print(colored('3...', 'red', 'on_red', ['bold']))
+print(colored('3...', 'white', 'on_red', ['bold']))
 time.sleep(1)
-print(colored('2...', 'red', 'on_red', ['bold']))
+print(colored('2...', 'white', 'on_red', ['bold']))
 time.sleep(1)
-print(colored('1...', 'red', 'on_red', ['bold']))
+print(colored('1...', 'white', 'on_red', ['bold']))
 time.sleep(1)
-print(colored('Quit this window to end the script!', 'blue', 'on_blue', ['bold']))
+print(colored('Quit this window to end the script!', 'white', 'on_blue', ['bold']))
 
 while True:
     # type message in chat
