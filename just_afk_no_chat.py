62b71d5ac0d3589ffa4c7653dd98fa0cd7aaf9e9
TheDog <130795558+TheDog-bot@users.noreply.github.com>
2023-10-26 07:11:31 -0400

    fix color

--- just_afk_no_chat.py
+++ just_afk_no_chat.py
@@ -3,19 +3,20 @@ import time
 import pyautogui
 from termcolor import colored
 
-print(colored('Make sure you are in a game! You have 30 before the script starts.', 'green', 'on_green', ['bold', 'underline']))
+print(colored('Made by @thedog-bot on github.', 'white', 'on_green', ['bold', 'underline']))
+print(colored('Make sure you are in a game! You have 30 before the script starts.', 'white', 'on_yellow', ['bold', 'underline']))
 time.sleep(25)
-print(colored('5...', 'red', 'on_red', ['bold']))
+print(colored('5...', 'white', 'on_red', ['bold']))
 time.sleep(1)
-print(colored('4...', 'red', 'on_red', ['bold']))
+print(colored('4...', 'white', 'on_red', ['bold']))
 time.sleep(1)
-print(colored('3...', 'red', 'on_red', ['bold']))
+print(colored('3...', 'white', 'on_red', ['bold']))
 time.sleep(1)
-print(colored('2...', 'red', 'on_red', ['bold']))
+print(colored('2...', 'white', 'on_red', ['bold']))
 time.sleep(1)
-print(colored('1...', 'red', 'on_red', ['bold']))
+print(colored('1...', 'white', 'on_red', ['bold']))
 time.sleep(1)
-print(colored('Quit this window to end the script!', 'yellow', 'on_yellow', ['bold']))
+print(colored('Quit this window to end the script!', 'white', 'on_blue', ['bold']))
 while True:
     pyautogui.press("space")
     sleep_time = random.randint(10, 40)
